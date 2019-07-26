33f19467a61e4348672af3a8a6c473a259ad533d
unknown <huafen.sun@gxever.cc>
2019-07-26 14:37:43 +0800

    图片修改

--- miniprogram/pages/photo/photo.js
+++ miniprogram/pages/photo/photo.js
@@ -3,21 +3,7 @@
 Page({
 
   data: {
-    imgSrc: [
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/2.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/3.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/4.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/5.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/6.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/7.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/8.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/9.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/10.png',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/11.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/12.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/13.jpg',
-      'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/14.jpg'
-    ],
+    imgPath: 'cloud://wedding-e018a3.7765-wedding-e018a3/wedding/photo/',
     isSwitch: false
   },
 
